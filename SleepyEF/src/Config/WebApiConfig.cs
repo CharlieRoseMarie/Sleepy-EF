140e38b5d0a775b30969f7b9bb7647a5f981b238
Charlotte Allen <CharlotteRMAllen@gmail.com>
2018-07-18 12:42:44 -0700

    rename and organization

---
+++ SleepyEF/src/Config/WebApiConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+using Microsoft.AspNet.OData.Builder;
+
+namespace SleepyEF.EntityFrameworkCore.Dynamics.Config
+{
+    public class WebApiConfig
+    {
+        public void Register(HttpConfiguration config)
+        {
+            var builder = new ODataConventionModelBuilder();
+        }
+    }
+}
---
+++ SleepyEF/src/Database/Storage/DynamicsDatabaseProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace SleepyEF.EntityFrameworkCore.Dynamics.Database.Storage
+{
+    public class DynamicsDatabaseProvider : IDatabaseProvider
+    {
+        public bool IsConfigured(IDbContextOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name { get; }
+    }
+}
